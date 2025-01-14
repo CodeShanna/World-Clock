0d9b94c661b90c312f6acda8fd31917320abd6e9
Shanna Jones <shanna@jonesbunch.com>
2025-01-14 11:20:19 +0100

    JS functionality to update Timezones

--- src/index.js
+++ src/index.js
@@ -1,33 +1,59 @@
 function updateTime() {
   let tubingenElement = document.querySelector("#tubingen");
-  let tubingenDateElement = tubingenElement.querySelector(".date");
-  let tubingenTimeElement = tubingenElement.querySelector(".time");
-  let tubingenTime = moment().tz("Europe/Berlin");
+  if (tubingenElement) {
+    let tubingenDateElement = tubingenElement.querySelector(".date");
+    let tubingenTimeElement = tubingenElement.querySelector(".time");
+    let tubingenTime = moment().tz("Europe/Berlin");
 
-  tubingenDateElement.innerHTML = tubingenTime.format("MMMM Do YYYY");
-  tubingenTimeElement.innerHTML = tubingenTime.format(
-    "h:mm ss [<small>]A[</small>]"
-  );
+    tubingenDateElement.innerHTML = tubingenTime.format("MMMM Do YYYY");
+    tubingenTimeElement.innerHTML = tubingenTime.format(
+      "h:mm ss [<small>]A[</small>]"
+    );
+  }
 
   let eufalaElement = document.querySelector("#eufala");
-  let eufalaDateElement = eufalaElement.querySelector(".date");
-  let eufalaTimeElement = eufalaElement.querySelector(".time");
-  let eufalaTime = moment().tz("America/North_Dakota/New_Salem");
+  if (eufalaElement) {
+    let eufalaDateElement = eufalaElement.querySelector(".date");
+    let eufalaTimeElement = eufalaElement.querySelector(".time");
+    let eufalaTime = moment().tz("America/North_Dakota/New_Salem");
 
-  eufalaDateElement.innerHTML = eufalaTime.format("MMMM Do YYYY");
-  eufalaTimeElement.innerHTML = eufalaTime.format(
-    "h:mm ss[<small>]A[</small>]"
-  );
+    eufalaDateElement.innerHTML = eufalaTime.format("MMMM Do YYYY");
+    eufalaTimeElement.innerHTML = eufalaTime.format(
+      "h:mm ss[<small>]A[</small>]"
+    );
+  }
 
   let eastlakeElement = document.querySelector("#eastlake");
-  let eastlakeDateElement = eastlakeElement.querySelector(".date");
-  let eastlakeTimeElement = eastlakeElement.querySelector(".time");
-  let eastlakeTime = moment().tz("America/Denver");
+  if (eastlakeElement) {
+    let eastlakeDateElement = eastlakeElement.querySelector(".date");
+    let eastlakeTimeElement = eastlakeElement.querySelector(".time");
+    let eastlakeTime = moment().tz("America/Denver");
 
-  eastlakeDateElement.innerHTML = eastlakeTime.format("MMMM Do YYYY");
-  eastlakeTimeElement.innerHTML = eastlakeTime.format(
-    "h:mm ss [<small>]A[</small>]"
-  );
+    eastlakeDateElement.innerHTML = eastlakeTime.format("MMMM Do YYYY");
+    eastlakeTimeElement.innerHTML = eastlakeTime.format(
+      "h:mm ss [<small>]A[</small>]"
+    );
+  }
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format("h:mm ss")}<small>${cityTime.format(
+    "A"
+  )}</small></div>
+    </div>
+  `;
+}
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
